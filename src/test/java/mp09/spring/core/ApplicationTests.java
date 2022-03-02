1599ac62c40705ab72bf446e3f5f5abe162f3b5e
farreladitya <farelistihsanaditya@gmail.com>
2022-03-02 22:21:56 +0700

    first commit

---
+++ src/test/java/mp09/spring/core/ApplicationTests.java
@@ -0,0 +1,13 @@
+package mp09.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
