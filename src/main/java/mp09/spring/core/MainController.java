d7c6e368721abfb3caf136bce08acd8d368935fb
farreladitya <farelistihsanaditya@gmail.com>
2022-03-05 15:49:12 +0700

    setup database & login page

---
+++ src/main/java/mp09/spring/core/MainController.java
@@ -0,0 +1,13 @@
+package mp09.spring.core;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("")
+    public String showHomePage(){
+        return "index";
+    }
+}
