027b5dd6493dbc4e14e1e37a01ca487a31523082
Rifqi Muhammad Aziz <rifqimuhammadaziz@gmail.com>
2022-03-06 14:19:56 +0700

    create entity & add property

---
+++ src/main/java/mp09/spring/core/entity/Product.java
@@ -0,0 +1,58 @@
+package mp09.spring.core.entity;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private String category;
+    private String price;
+    private String description;
+
+    public Product(int id, String name, String category, String price, String description) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
